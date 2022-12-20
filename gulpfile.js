63f8f7e473fe00099b800087e219b4ecab6e5613
jitendra32 <jitendra@itvedant.com>
2022-12-20 13:35:58 +0530

    practice gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'); 
+var concat = require('gulp-concat'); 
+var minify = require('gulp-minify'); 
+var cleanCSS = require('gulp-clean-css');
+
+
+gulp.task('allJs', function(){    
+    return gulp.src([
+        'js/plugin.js',
+        'js/slider-setting.js',
+    ])          
+        .pipe(concat('all.js'))
+        .pipe(minify())       
+        .pipe(gulp.dest('dest/js')); 
+});
+
+
+gulp.task('minify', () => {
+    return gulp.src([
+        'css/slick.css',
+    ])
+    .pipe(concat('all.css'))
+    .pipe(cleanCSS())       
+    .pipe(gulp.dest('dest/css')); 
+  });
+  
